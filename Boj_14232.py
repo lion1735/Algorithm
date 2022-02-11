aa41e3d84a462963975584a0e12b96af5660bbb1
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-02-11 22:05:51 +0900

    Create Boj_14232.py

---
+++ Boj_14232.py
@@ -0,0 +1,15 @@
+n = int(input())
+res = []
+cnt =0
+
+for x in range(2,int(n**0.5)+2):
+    if x > n:
+        break
+    while n%x==0:
+        res.append(x)
+        n//=x
+        cnt+=1
+if n!=1:
+    res.append(n)
+print(len(res))
+print(*res)
