4168b1223e5fa743030bd5a15104431a472cc918
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-03-03 18:29:04 +0900

    Create 1654.py

---
+++ 1654.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+li = [int(input()) for i in range(n)]
+
+l = 1
+r = max(li)
+while l <= r:
+    cnt = 0
+    mid = (l + r)//2
+    for x in li:
+        cnt += x//mid
+    if cnt >= m:
+        l = mid + 1
+    else:
+        r = mid - 1
+print(r)
+
