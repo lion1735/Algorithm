900811eb3adda6b27260ac3532462ecbfab49e82
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-03-09 18:28:01 +0900

    Create Boj_1764.py

---
+++ Boj_1764.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+liA = set()
+liB = set()
+for i in range(n):
+    liA.add(input())
+for i in range(m):
+    liB.add(input())
+res = sorted(list(liA & liB))
+print(len(res))
+for x in res:
+    print(x)
