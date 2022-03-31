d025b7e21d8ab8e34a647b11b4fc2503cfa517f0
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-03-31 17:42:52 +0900

    Create Boj_1149.java

---
+++ Boj_1149.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main_1149_최강 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int cost[][] = new int[n + 1][3];
+        int dp[][] = new int[n + 1][3];
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            for (int j = 0; j < 3; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
+        }
+        System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
+    }
+}
