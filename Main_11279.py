4620c25e7681b471ec7c10137fd855f2b6351787
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-04-12 17:18:51 +0900

    최대힙 -  heapq 사용법

---
+++ Main_11279.py
@@ -0,0 +1,13 @@
+import heapq
+import sys
+n = int(sys.stdin.readline())
+heap = []
+for i in range(n):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        if len(heap) > 0:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)    
+    else:
+        heapq.heappush(heap, (-num, num))
