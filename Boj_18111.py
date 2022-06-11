944ac0366015e475b47bebfdde25e905ec767927
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-06-11 16:18:05 +0900

    Update Boj_18111.py

--- Boj_18111.py
+++ Boj_18111.py
@@ -2,6 +2,7 @@
 # for i in range(257)은 항상 i가 오름차순으로 돌기 때문에
 # 시간이 같아도 최종적으로는 높이가 높은 순으로 나오게 된다
 # 257 까지 모두 반복해서 땅을 채워본 다음에 가장 작은 값을 저장
+
 n, m, b = map(int, input().split())
 table = [list(map(int, input().split())) for _ in range(n)]
 height = 0
