52f933133388891ae361ea958b3ff0c850997c2a
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-06-18 22:30:24 +0900

    Update Boj_1149.java

--- Boj_1149.java
+++ Boj_1149.java
@@ -22,7 +22,7 @@ public class Main_1149_최강 {
             dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
             dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
         }
-        System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
+        System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2])); 
     }
 }
  
